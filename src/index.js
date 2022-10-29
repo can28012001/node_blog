536fdf8d7cc1c948079fe52a7006f663229858cb
ngoccan <dongoccan9b@gmail.com>
2022-10-30 00:40:21 +0700

    prettier_update1

--- src/index.js
+++ src/index.js
@@ -41,13 +41,13 @@ app.get('/news', (req, res) => {
     res.render('news');
 });
 
-app.get('/search', (req, res) => {
-    res.render('search');
+ app.get('/search', (req, res) => {
+        res.render('search');
 });
 
 // 127.0.0.1 - localhost
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+       console.log(`Example app listening on port ${port}`);
 });
 
 // var express = require('express');
